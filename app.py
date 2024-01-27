c3efe691f0acb78f30ece29b5f8ab884a3e8992a
arun <arunkumar39874@gmail.com>
2024-01-27 18:18:42 +0530

    Commit

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "127.0.0.1", port = 8080, debug=True)
